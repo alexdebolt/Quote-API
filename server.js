ad6fa41926d35ba3ecd7f19a412531770883a447
Alex DeBolt <58086131+alexdebolt@users.noreply.github.com>
2020-01-28 13:43:38 -0500

    Add get handler for all quotes and quotes by person

--- server.js
+++ server.js
@@ -8,16 +8,30 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
-const quotesRouter = express.Router();
-app.use('/api/quotes', quotesRouter);
+// gets a random quote from quotes array in data.js
+app.get('/api/quotes/random', (req, res, next) => {
+    res.send({
+        quote: getRandomElement(quotes)
+    });
+});
 
-quotesRouter.get('/random', (req, res, next) => {
-    let randomQuote = getRandomElement(quotes);
-    if(randomQuote) {
-        res.send({quote: randomQuote});
-    } else {
-        res.status(404).send();
+// returns all quotes if no query params and return all quotes by a specific person
+app.get('/api/quotes', (req, res, next) => {
+    //if a person is specified, filter and return only quotes by that person
+    if(req.query.person != undefined) {
+        const quotesByPerson = quotes.filter(quote => quote.person === req.query.person);
+        res.send({
+            quotes: quotesByPerson
+        })
+    } 
+    // returns all quotes 
+    else {
+        res.send({
+            quotes: quotes
+        });
     }
-})
+});
+
+
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
